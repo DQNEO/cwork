176a2680cb707b5de9bc5084cf7da9b741248fa4
DQNEO <daisuke0928@gmail.com>
2011-05-13 01:46:52 +0900

    文字列の配列！！ポインタ編！！

---
+++ 12_array_of_array.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+main()
+{
+  char* Meibo[5] = {"apple", "mango", "orange", "mikan", "banana" };
+
+  printf("%s\n", Meibo[0]);
+  printf("%s\n", Meibo[1]);
+  printf("%s\n", Meibo[2]);
+  printf("%s\n", Meibo[3]);
+  printf("%s\n", Meibo[4]);
+}
