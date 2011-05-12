e2e8b3d5194872e795f9292e01366d722f861687
DQNEO <daisuke0928@gmail.com>
2011-05-13 01:47:32 +0900

    文字列の配列！！2次元配列で！！

--- 11_array_of_array.c
+++ 11_array_of_array.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 main()
 {
   char Meibo[5][20] = {"apple", "mango", "orange", "doragon fruites", "mikan" } ;
@@ -9,5 +8,13 @@ main()
   printf("%s\n", &Meibo[2][0]);
   printf("%s\n", &Meibo[3][0]);
   printf("%s\n", &Meibo[4][0]);
+
+  printf("=====\n");
+
+  printf("%s\n", Meibo[0]);
+  printf("%s\n", Meibo[1]);
+  printf("%s\n", Meibo[2]);
+  printf("%s\n", Meibo[3]);
+  printf("%s\n", Meibo[4]);
 
 }
