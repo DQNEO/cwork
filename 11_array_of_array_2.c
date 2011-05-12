bd966b4d7cd428c884445eae8643e6cc195e70fb
DQNEO <daisuke0928@gmail.com>
2011-05-12 21:01:40 +0900

    2次元配列！！ アドレス渡し！

---
+++ 11_array_of_array_2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+main()
+{
+  char Meibo[5][20] = {"apple", "mango", "orange", "doragon fruites", "mikan" } ;
+
+  printf("%s\n", Meibo[0]);
+  printf("%s\n", Meibo[1]);
+  printf("%s\n", Meibo[2]);
+  printf("%s\n", Meibo[3]);
+  printf("%s\n", Meibo[4]);
+
+}
