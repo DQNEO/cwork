50635d56224e2dfe4c0c379ff0344becb4c4447e
DQNEO <daisuke0928@gmail.com>
2011-05-12 15:40:23 +0900

    re-invent "cat" command

--- 07_cat.c
+++ 07_cat.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h> /* exit */
 main(int argc, char *argv[])
 {
 
   FILE *fin;
   char s[256];
 
-  fin = fopen(argv[1], "r");
+  if ((fin = fopen(argv[1], "r")) == NULL) {
+    printf("cannot open file\n");
+    exit(1);
+  }
 
-  while( fgets(s, 256,fin) != NULL) {
+      while (fgets(s, 256,fin) != NULL) {
     printf("%s",s);
   }
 
