73c1d96f879c1842d0f1ca329b954463d90c4665
DQNEO <DQNEOO@gmail.com>
2012-10-21 16:12:22 +0000

    refactoring

--- 11_array_of_array.c
+++ 11_array_of_array.c
@@ -6,20 +6,15 @@ main()
 {
     /* 文字列の配列を初期化 */
     /* 20ってのがちょっとダサイので何とかしたい */
-    char Meibo[5][20] = {"apple", "mango", "orange", "banana", "mikan" } ;
+    char Meibo[5][20] = {"apple", "mango", "orange"} ;
 
     printf("%s\n", &Meibo[0][0]);
     printf("%s\n", &Meibo[1][0]);
     printf("%s\n", &Meibo[2][0]);
-    printf("%s\n", &Meibo[3][0]);
-    printf("%s\n", &Meibo[4][0]);
 
-    printf("=====\n");
-
+    printf("\n");
+    //下記のように書いても同じである。
     printf("%s\n", Meibo[0]);
     printf("%s\n", Meibo[1]);
     printf("%s\n", Meibo[2]);
-    printf("%s\n", Meibo[3]);
-    printf("%s\n", Meibo[4]);
-
 }
