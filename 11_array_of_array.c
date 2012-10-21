da7babbd2aa8042b5d91b915d3c53e302dc075f4
DQNEO <DQNEOO@gmail.com>
2012-10-21 16:09:34 +0000

    indent

--- 11_array_of_array.c
+++ 11_array_of_array.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 main()
 {
-  char Meibo[5][20] = {"apple", "mango", "orange", "banana", "mikan" } ;
+    char Meibo[5][20] = {"apple", "mango", "orange", "banana", "mikan" } ;
 
-  printf("%s\n", &Meibo[0][0]);
-  printf("%s\n", &Meibo[1][0]);
-  printf("%s\n", &Meibo[2][0]);
-  printf("%s\n", &Meibo[3][0]);
-  printf("%s\n", &Meibo[4][0]);
+    printf("%s\n", &Meibo[0][0]);
+    printf("%s\n", &Meibo[1][0]);
+    printf("%s\n", &Meibo[2][0]);
+    printf("%s\n", &Meibo[3][0]);
+    printf("%s\n", &Meibo[4][0]);
 
-  printf("=====\n");
+    printf("=====\n");
 
-  printf("%s\n", Meibo[0]);
-  printf("%s\n", Meibo[1]);
-  printf("%s\n", Meibo[2]);
-  printf("%s\n", Meibo[3]);
-  printf("%s\n", Meibo[4]);
+    printf("%s\n", Meibo[0]);
+    printf("%s\n", Meibo[1]);
+    printf("%s\n", Meibo[2]);
+    printf("%s\n", Meibo[3]);
+    printf("%s\n", Meibo[4]);
 
 }
