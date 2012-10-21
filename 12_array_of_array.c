7525d5bad7e26809bacbe2313b742eef21d625c4
DQNEO <DQNEOO@gmail.com>
2012-10-21 16:14:12 +0000

    refactoring

--- 12_array_of_array.c
+++ 12_array_of_array.c
@@ -3,7 +3,7 @@ main()
 {
     char* Meibo[5] = {"apple", "mango", "orange"};
 
-  printf("%s\n", Meibo[0]);
-  printf("%s\n", Meibo[1]);
-  printf("%s\n", Meibo[2]);
+    printf("%s\n", Meibo[0]);
+    printf("%s\n", Meibo[1]);
+    printf("%s\n", Meibo[2]);
 }
