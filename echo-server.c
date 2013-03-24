780c4884bdd0443dee547f434b09eb8aa4135186
DQNEOO <DQNEOO@gmail.com>
2013-03-24 04:12:36 +0000

    sample code of TCP server
    original from http://x68000.q-e-d.net/~68user/net/sample/echo-server-1.c

---
+++ echo-server.c
@@ -0,0 +1,148 @@
+/*
+ * $Id: echo-server-1.c,v 1.6 2005/02/19 16:01:53 68user Exp $
+ *
+ * sample of echo server
+ *
+ * original from http://x68000.q-e-d.net/~68user/net/sample/echo-server-1.c
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <sys/param.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+/* buffer length */
+#define BUF_LEN 256
+
+/* ソケット socket から1行読み込み、読み込んだ文字列を p に格納する。
+   改行コードを読み込む前にソケットから read できなくなった場合は、
+   その時点で呼び出し元に戻る。
+
+   戻り値で読み込んだ文字数を返す。p は \0 でターミネートする。
+*/
+int read_line(int socket, char *p){
+  int len = 0;
+  while (1){
+    int ret;
+    ret = read(socket, p, 1);
+    if ( ret == -1 ){
+      perror("read");
+      exit(1);
+    } else if ( ret == 0 ){
+      break;
+    }
+    if ( *p == '\n' ){
+      p++;
+      len++;
+      break;
+    }
+    p++;
+    len++;
+  }
+  *p = '\0';
+  return len;
+}
+
+
+int main(int argc, char *argv[]){
+  int connected_socket, listening_socket;
+  struct sockaddr_in sin;
+  int len, ret;
+  int sock_optval = 1;
+  int port = 5000;
+
+  /* create a listening_socket */
+  listening_socket = socket(AF_INET, SOCK_STREAM, 0);
+  if ( listening_socket == -1 ){
+    perror("socket");
+    exit(1);
+  }
+
+  /* set socket option */
+  if ( setsockopt(listening_socket, SOL_SOCKET, SO_REUSEADDR,
+		  &sock_optval, sizeof(sock_optval)) == -1 ){
+    perror("setsockopt");
+    exit(1);
+  }
+
+  /* address family, port number, IP address */
+  sin.sin_family = AF_INET;
+  sin.sin_port = htons(port);
+  sin.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  /* bind */
+  if ( bind(listening_socket, (struct sockaddr *)&sin, sizeof(sin)) < 0 ){
+    perror("bind");
+    exit(1);
+  }
+  /* listen */
+  ret = listen(listening_socket, SOMAXCONN);
+  if ( ret == -1 ){
+    perror("listen");
+    exit(1);
+  }
+  printf("listening port %d ...\n", port);
+
+  while (1){
+    struct hostent *peer_host;
+    struct sockaddr_in peer_sin;
+
+    len = sizeof(peer_sin);
+
+    /* accept */
+    connected_socket = accept(listening_socket, (struct sockaddr *)&peer_sin, &len);
+    if ( connected_socket == -1 ){
+      perror("accept");
+      exit(1);
+    }
+
+    /* get client host info */
+    peer_host = gethostbyaddr((char *)&peer_sin.sin_addr.s_addr,
+			      sizeof(peer_sin.sin_addr), AF_INET);
+    if ( peer_host == NULL ){
+      printf("gethostbyname failed\n");
+      exit(1);
+    }
+
+    printf("conection:%s [%s] port %d\n",
+	   peer_host->h_name,
+	   inet_ntoa(peer_sin.sin_addr),
+	   ntohs(peer_sin.sin_port)
+	   );
+
+    while (1){
+      int read_size;
+      char buf[BUF_LEN];
+
+      read_size = read_line(connected_socket, buf);
+      if ( read_size == 0 ) break;
+
+      printf("message: %s", buf);
+
+      /* echo back */
+      write(connected_socket, buf, strlen(buf));
+    }
+
+    printf("connection closed. listening port %d ...\n", port);
+    ret = close(connected_socket);
+    if ( ret == -1 ){
+      perror("close");
+      exit(1);
+    }
+  }
+  ret = close(listening_socket);
+  if ( ret == -1 ){
+    perror("close");
+    exit(1);
+  }
+
+  return 0;
+}
