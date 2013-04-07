ed498be0918a604e3597c176263d2d3b901bcdfc
DQNEO <dqneoo@gmail.com>
2013-04-07 21:28:30 +0900

    構造体をコピーして別々のインスタンスであることを確認する実験

---
+++ neko/9_2_structcopy.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/* 構造体をコピーする */
+
+int main()
+{
+    struct DATA {
+        int x;
+        int y;
+    } a = {10,20},b;
+    
+    //copy
+    b = a;
+    
+    printf("x = %d, y = %d\n", b.x, b.y);
+    
+    //aとbが別々のインスタンスであることを確認
+    b.x = 100;
+    printf("a.x = %d, b.x = %d\n", a.x, b.x);
+    
+    
+    //aとbのアドレスが異なることを確認
+    printf("address a is %p\n", &a);
+    printf("address b is %p\n", &b);
+}
