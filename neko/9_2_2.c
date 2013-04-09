0be995e8c4c09a18567230c7fad607ceea45f4fe
DQNEO <dqneoo@gmail.com>
2013-04-09 21:36:15 +0900

    refactor

--- neko/9_2_2.c
+++ neko/9_2_2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/* 構造体を宣言すると同時に初期化 */
+/* 構造体を宣言 */
 struct DATA {
     int data1;
     char data2;
@@ -9,6 +9,7 @@ struct DATA {
 
 int main()
 {
+    /* インスタンス宣言と同時に初期化 */
     struct DATA mydata = {10, 'A', "cat"};
     printf("%d, %c, %s\n",
         mydata.data1, mydata.data2, mydata.data3);
