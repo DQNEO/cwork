26804d8889aa6e49f97e0403f8d431fa7167ab49
DQNEO <dqneoo@gmail.com>
2013-04-10 22:30:47 +0900

    typedefを使う簡単なサンプルコード

---
+++ neko/9_5_type.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* typedef‚Ìg‚¢•û */
+/* SEISUŒ^‚ğV‚µ‚­éŒ¾‚·‚éB(À‘Ì‚ÍintŒ^‚Æ“¯‚¶) */
+
+typedef int SEISU;
+
+main()
+{
+    SEISU n;
+    
+    for(n=0;n<10;n++) printf("%d,", n);
+    
+    printf("\n");
+}
