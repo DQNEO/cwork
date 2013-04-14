c06b48fca77ca782cd234f77782feb4e66dc8615
DQNEO <dqneoo@gmail.com>
2013-04-14 20:29:34 +0900

    配列を宣言と同時に値を初期化する方法

---
+++ neko/8_1_2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int a[4] = {1,2,100,5};
+
+
+	printf("a[0] = %d\n", a[0]);
+	printf("a[1] = %d\n", a[1]);
+	printf("a[2] = %d\n", a[2]);
+	printf("a[3] = %d\n", a[3]);
+
+	return 0;
+}
+
