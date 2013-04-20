2ce8de1604b64c3a2016000374d3bc0d1b05a930
DQNEOO <DQNEOO@gmail.com>
2013-04-20 14:06:21 +0000

    共用体はどのメンバも同じ場所を指していることを示す実験。

--- union_struct.c
+++ union_struct.c
@@ -27,9 +27,15 @@ int main()
   printf("address s.d1 = %p\n", &(s.d1));
   printf("address s.d2 = %p\n", &(s.d2));
 
+  printf("\n");
+
   printf("address u.i1 = %p\n", &(u.i1));
   printf("address u.i2 = %p\n", &(u.i2));
   printf("address u.d1 = %p\n", &(u.d1));
   printf("address u.d2 = %p\n", &(u.d2));
-  
+
+  u.i1 = 100;
+  printf("value u.i1 = %d\n", u.i1);
+  printf("value u.i2 = %d\n", u.i2);
+
 }
