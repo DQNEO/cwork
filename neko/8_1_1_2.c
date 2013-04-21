c67aca499313a7f475f2165cfbfc6cb04a439032
DQNEOO <DQNEOO@gmail.com>
2013-04-21 17:19:27 +0000

    猫でもわかるC言語 第8章　配列(復習)

---
+++ neko/8_1_1_2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+  int a[4];
+
+  a[0] = 1;
+  a[1] = 2;
+  a[2] = 100;
+  a[3] = 5;
+
+  printf("a[0] = %d\n", a[0]);
+  printf("a[1] = %d\n", a[1]);
+  printf("a[2] = %d\n", a[2]);
+  printf("a[3] = %d\n", a[3]);
+  
+  return 0;
+}
