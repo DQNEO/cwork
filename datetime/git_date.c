9087a0f557cc4c01dbd8945e7b3dd67ade126b7d
DQNEO <dqneoo@gmail.com>
2013-10-05 22:55:10 +0900

    git commit log の日付フォーマットを再現中なう。
    年、曜日ができた。

---
+++ datetime/git_date.c
@@ -0,0 +1,58 @@
+/**
+ * Gitのコミットログの日付フォーマット処理を再現する
+ * e.g. "Sat Oct 5 18:42:19 2013 +0900"
+ * 
+ * date.cのshow_dateのコードをマネした。
+ */
+#include <stdio.h>
+#include <time.h>
+
+char *weekday_names[] = {
+  "Sunday",
+  "Mondays",
+  "Tuesdays",
+  "Wednesdays",
+  "Thursdays",
+  "Fridays",
+  "Saturdays"
+};
+
+char *month_names[] = {
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+};
+
+
+int main()
+{
+  time_t t;
+  struct tm *ptm;
+  t = 1380980291;
+  ptm = localtime(&t);
+
+  printf("tm_wday = %d\n", ptm->tm_wday);
+  printf("tm_mon = %d\n", ptm->tm_mon);
+  printf("tm_mday = %d\n", ptm->tm_mday);
+  printf("(tm_hour, tm_min, tm_sec) = (%d, %d, %d)\n",
+	 ptm->tm_hour, ptm->tm_min, ptm->tm_sec);
+  printf("tm_year + 1900 = %d\n", ptm->tm_year + 1900);
+  printf("=============\n");
+
+
+  printf("%.3s %.3s\n",
+	 weekday_names[ptm->tm_wday],
+	 month_names[ptm->tm_mon]
+	 );
+  
+  return 0;
+}
