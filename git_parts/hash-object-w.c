b6e7575c1e87b49c2bb460cb4600607e0a0451fc
DQNEO <dqneoo@gmail.com>
2013-10-08 23:37:41 +0900

    refactor

--- git_parts/hash-object-w.c
+++ git_parts/hash-object-w.c
@@ -10,18 +10,24 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 
-
-int main(int argc, char **argv) {
-    printf("%s\n", argv[1]);
-
-    char *filename = argv[1];
-
+void hash_object_w(char *filename)
+{
     struct stat st;
     if (lstat(filename, &st)) {
 	fprintf(stderr, "unable to stat %s\n", filename);
 	exit(1);
     }
     printf("st.size = %ld\n", st.st_size);
+
+}
+
+
+int main(int argc, char **argv)
+{
+    printf("%s\n", argv[1]);
+
+    char *filename = argv[1];
+    hash_object_w(filename);
     return 0;
 }
 
